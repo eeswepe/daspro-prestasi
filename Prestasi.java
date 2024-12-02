6391682b755c12e7695c50fdd142cb492a683968
eeswepe <singgihwahyupermana@gmail.com>
2024-12-02 14:19:19 +0700

    feat: initial template

---
+++ Prestasi.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Prestasi {
+
+    public static void inputPrestasi(int arr[][]){
+
+    }
+
+    public static void outputPrestasi(int arr[][]){
+        
+    }
+
+    public static String[] analisisPrestasi(int arr[][]){
+        return null;
+    }
+
+    public static void main(String[] args) {
+        String prestasi[][] = new String[5][];
+    }    
+}
