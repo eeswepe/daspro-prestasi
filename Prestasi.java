835b0dcf9541eea73113c6de7294ec5246c51b7e
eeswepe <singgihwahyupermana@gmail.com>
2024-12-04 14:31:30 +0700

    feat: add arguments

--- Prestasi.java
+++ Prestasi.java
@@ -2,15 +2,15 @@ import java.util.Scanner;
 
 public class Prestasi {
 
-    public static void inputPrestasi(String arr[][]){
+    public static void inputPrestasi(String arr[][], int tahun_prestasi[]){
 
     }
 
-    public static void outputPrestasi(String arr[][]){
+    public static void outputPrestasi(String arr[][], int tahun_prestasi[]){
         
     }
 
-    public static String[] analisisPrestasi(String arr[][]){
+    public static String[] analisisPrestasi(String arr[][], int tahun_prestasi[]){
         return null;
     }
 
